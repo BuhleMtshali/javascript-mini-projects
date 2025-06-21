12c45419b5f6c52b702a2a413606e73ece065c64
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-21 18:22:12 +0200

    updated the variable file

--- dayOne-projects/variableIntro.js
+++ dayOne-projects/variableIntro.js
@@ -1,5 +1,10 @@
 let userName = prompt("Enter your name: ");
 let userAge = prompt("Enter your current age: ");
-let age = 18;
 
-console.log("==== Welcome to Club Athena🏢 ====")
+
+console.log("==== Welcome to Club Athena🏢 ====");
+if (userAge >= 18){
+    console.log(`Welcome ${userName}, you're currently ${userAge} years old which means you can enter the club!!`);
+} else {
+    console.log(`Well this is a bummer ${userName}, its seems you're ${userAge} years old which means you're below the minimum age of 18!!`)
+}
