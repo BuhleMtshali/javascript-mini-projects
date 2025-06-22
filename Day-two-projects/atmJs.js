7a3b050dbcb4334bf69a19a932e0cc1b4420b7bc
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-22 06:35:30 +0200

    updated the atm js

---
+++ Day-two-projects/atmJs.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+
+
+//defining variables
+let balance = 1000;
+let choice;
+
+console.log("====== Welcome to JS Bank🏦 =======")
+let name = prompt("Enter your name: ");
+
+//welcome message
+console.log(`====== Welcome to JavaScript Bank🏦 ${name} =====`);
