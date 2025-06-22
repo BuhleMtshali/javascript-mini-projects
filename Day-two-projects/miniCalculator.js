2f1fd8ee20c20c9165e52e9f4338798434da7655
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-22 09:21:31 +0200

    updated the mini calculator

--- Day-two-projects/miniCalculator.js
+++ Day-two-projects/miniCalculator.js
@@ -1,11 +1,34 @@
 const prompt = require("prompt-sync")();
 
-console.log("======= Welcome to Mini Calculator🧮 =====");
+console.log("\n======= Welcome to Mini Calculator🧮 =====");
+let result;
 
 function calculatorProgram(){
-   let valueOne = parseFloat(prompt('Enter your first value'));
+   let valueOne = parseFloat(prompt('Enter your first value: '));
    let operator = prompt("Enter an Operator(+, -, *, /): ");
    let secondValue = parseFloat(prompt("Enter your second value: "));
+
+   switch(operator){
+    case '+':
+        result = valueOne + secondValue;
+        console.log(`Result: ${result}`);
+        break;
+    case '-':
+        result = valueOne - secondValue;
+        console.log(`Result: ${result}`);
+        break;
+    case '*':
+        result = valueOne * secondValue;
+        console.log(`Result: ${result}`);
+        break;
+    case '/':
+        result = valueOne / secondValue;
+        console.log(`Result: ${result}`);
+        break;
+    default:
+        console.log(`You have entered an invalid ${operator}`)
+
+   }
 }
 
 calculatorProgram()
