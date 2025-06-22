0b86bacd13f515c1566aa23b72694da34cca1968
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-22 09:15:15 +0200

    updated the calculator program

--- Day-two-projects/miniCalculator.js
+++ Day-two-projects/miniCalculator.js
@@ -3,5 +3,9 @@ const prompt = require("prompt-sync")();
 console.log("======= Welcome to Mini Calculator🧮 =====");
 
 function calculatorProgram(){
-   
+   let valueOne = parseFloat(prompt('Enter your first value'));
+   let operator = prompt("Enter an Operator(+, -, *, /): ");
+   let secondValue = parseFloat(prompt("Enter your second value: "));
 }
+
+calculatorProgram()
