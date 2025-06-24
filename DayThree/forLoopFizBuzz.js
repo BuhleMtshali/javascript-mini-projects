6638d27e151c9d7782769d07b501dbff6ecffc2d
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-24 23:21:07 +0200

    updated the forLoop

--- DayThree/forLoopFizBuzz.js
+++ DayThree/forLoopFizBuzz.js
@@ -1,13 +1,15 @@
 function fizzBuzz(){
-    for(let i = 0; i <= 15; i++){
-        if(i % 3){
+    for(let i = 1; i <= 15; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log(`${i} is FizzBuzz!!`);
+        } else if(i % 3 === 0){
             console.log(`${i} is Fizz!!!`);
-        } else if(i % 5){
-            console.log(`${i} is Buzz!!`)
-        } else if(i % 3 && i % 5){
-            console.log(`${i} is FizzBuzz!!`)
+        } else if(i % 5 === 0){
+            console.log(`${i} is Buzz!!`);
         } else {
-            console.log(i)
+            console.log(i);
         }
     }
 }
+
+fizzBuzz();
