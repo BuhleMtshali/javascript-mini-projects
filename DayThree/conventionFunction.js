2d28a18650bfaf8c4aec7bc52024f072b6b887a0
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-24 22:27:05 +0200

    updated the function

--- DayThree/conventionFunction.js
+++ DayThree/conventionFunction.js
@@ -3,7 +3,13 @@ const prompt = require("prompt-sync")();
 let celsius = parseFloat(prompt("Enter the tempereture (in celsius): "));
 
 function convertTemp(celsius){
-    return console.log((celsius * 9/4) + 32); 
+    if(!isNaN(celsius)){
+        let ferenheit = (celsius * 9/5) + 32;
+        return console.log(`The tempreture ${celsius}ºC is ${ferenheit.toFixed(2)}ºF`);
+    } else {
+        console.log(`${celsius} is an invalid number`)
+    }
+    
 }
 
 convertTemp(celsius)
