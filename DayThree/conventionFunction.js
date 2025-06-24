cd97005bc533e03336c2f497be359a8f3f3b48b3
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-24 22:17:56 +0200

    updated the convertion function

--- DayThree/conventionFunction.js
+++ DayThree/conventionFunction.js
@@ -1,7 +1,9 @@
+const prompt = require("prompt-sync")();
+
 let celsius = parseFloat(prompt("Enter the tempereture (in celsius): "));
 
 function convertTemp(celsius){
-    return (celsius * 9/4) + 32; 
+    return console.log((celsius * 9/4) + 32); 
 }
 
 convertTemp(celsius)
